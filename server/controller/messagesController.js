629ac2ebdbca040493d133d9943ee5d7a94333d7
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2024-09-26 15:46:28 -0300

    fix/get messages query

--- server/controller/messagesController.js
+++ server/controller/messagesController.js
@@ -2,7 +2,6 @@ const con = require('../connect/dbconnect');
 
 exports.sendMessage = async (req, res) => {
   const { userId, mensagem } = req.body;
-
   if (!userId || !mensagem) {
     return res.status(400).json({ error: 'User ID and message are required' });
   }
@@ -16,15 +15,19 @@ exports.sendMessage = async (req, res) => {
     res.status(201).json({ id: results.insertId });
   });
 };
-
 exports.getMessage = async (req, res) => {
   try {
     const query = 'SELECT u.username, m.mensagem, m.data FROM mensagens m JOIN users u ON m.id_user = u.id ORDER BY m.data DESC';
-    con.query(query, results => {
-      return !results ? res.json([]) : res.json(results)
-    })
+    con.query(query, (error, results) => {
+      if (error) {
+        console.error("Erro na execução da query:", error.message);
+        return res.status(500).json({ error: 'Erro ao consultar o banco de dados: ' + error.message });
+      }
+      return results.length === 0 ? res.json([]) : res.json(results);
+    });
+    
   } catch (error) {
-    console.error(error.message)
+    console.error(error.message);
     res.status(500).json({ error: 'Internal server error: ' + error.message });
   }
 };
