05fea847e98311703738caa6fcb5168fea2b29e8
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2024-11-02 09:16:02 -0300

    Feature/.env on dbConnection

--- server/connect/dbCreate.js
+++ server/connect/dbCreate.js
@@ -5,9 +5,9 @@ const { insertDefaultValues, insertDefaulConditions, insertDefaultUser } = requi
 (async () => {
     try {
         const con = await mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "senha",
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
         });
 
         // Criar banco de dados 'lgpd'
--- server/connect/dbconnect.js
+++ server/connect/dbconnect.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "senha",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "lgpd"
 });
 
