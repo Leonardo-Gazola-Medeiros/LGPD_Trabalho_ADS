9983877ea6715fbea973e66c636b33864e6bc9d9
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2024-11-02 09:16:07 -0300

    Update dbDrop.js

--- server/connect/dbDrop.js
+++ server/connect/dbDrop.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql2');
 
 var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "senha",
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 });
 
 con.connect(function(err) {
